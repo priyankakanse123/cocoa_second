31560c59d511c4d14d386832d77948396c782166
priyankakanse123 <priyankakanse93@gmail.com>
2016-01-29 12:50:41 +0530

    first push

---
+++ cocoa_second/editName.h
@@ -0,0 +1,13 @@
+//
+//  editName.h
+//  cocoa_second
+//
+//  Created by BridgeLabz on 18/12/15.
+//  Copyright © 2015 BridgeLabz. All rights reserved.
+//
+
+#ifndef editName_h
+#define editName_h
+
+
+#endif /* editName_h */
---
+++ cocoa_second/editName.m
@@ -0,0 +1,9 @@
+//
+//  editName.m
+//  cocoa_second
+//
+//  Created by BridgeLabz on 18/12/15.
+//  Copyright © 2015 BridgeLabz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
